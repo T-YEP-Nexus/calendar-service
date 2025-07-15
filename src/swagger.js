c3de35f75c17ca929fbd859fde6d1974e30eae24
Lucie Aloccio <114408320+luciealc@users.noreply.github.com>
2025-07-15 16:17:39 +0200

    feat/docs: swagger documentation for calendar microservice

---
+++ src/swagger.js
@@ -0,0 +1,21 @@
+const swaggerJSDoc = require('swagger-jsdoc');
+
+const options = {
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'Calendar API',
+      version: '1.0.0',
+      description: 'API for managing events and student registrations to specific events',
+    },
+    servers: [
+      {
+        url: 'http://localhost:3002',
+      },
+    ],
+  },
+  apis: ['./src/routes/*.js'],
+};
+
+const swaggerSpec = swaggerJSDoc(options);
+module.exports = swaggerSpec;
